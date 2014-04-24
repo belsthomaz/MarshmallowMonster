9d31d743f93c6d4a675faf73cda92bf2e6d03d64
belsthomaz <isabellazamoht@gmail.com>
2014-04-24 09:57:48 -0600

    added to controler

--- src/marsh/controller/MonsterController.java
+++ src/marsh/controller/MonsterController.java
@@ -1,10 +1,19 @@
 package marsh.controller;
 
+import marsh.model.Monster;
 import marsh.view.MonsterFrame;
 
 public class MonsterController
 {
 	private MonsterFrame appFrame;
+	private Monster [] monsters;
+	private Monster monsterCount;
+	
+	public MonsterController()
+	{
+		monsters = new Monster[4];
+		monsterCount = 0;
+	}
 	
 	public void start()
 	{
@@ -13,6 +22,15 @@ public class MonsterController
 	
 	public void addMonster(String name, int legs, int eyes, int arms, boolean hasHorns)
 	{
-		
+		if(monsterCount < monsters.length)
+		{
+			Monster tempMonster = new Monster(name, legs, eyes, arms, hasHorns);
+			monsters[monsterCount] = tempMonster;
+			monsterCount++;
+		}
+		else
+		{
+			
+		}
 	}
 }
